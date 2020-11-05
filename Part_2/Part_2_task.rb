54c0ccf72879a66799f7b40c52c88e87c2231a7d
kpkodil <kpkopb@gmail.com>
2020-11-05 21:45:29 +0300

    Update Part_2_task.rb
    Исправленое решение задания

--- Part_2/Part_2_task.rb
+++ Part_2/Part_2_task.rb
@@ -5,11 +5,12 @@ class Station
 	def initialize(name)
 		@name = name
 		@trains_list = []
+
 	end	
 
 	def show_trains
-		self.trains_list.each {|train| p train.name }
-		p ''
+		return self.trains_list.each {|train| p train.name }
+		
 	end
 
 	def show_trains_types
@@ -17,7 +18,7 @@ class Station
 		types_hash = Hash.new{0}
 		self.trains_list.each { |train| types_list << train.train_type}
 		types_list.each{ |type| types_hash[type] += 1 }
-		p types_hash
+		return types_hash
 	end
 
 	def train_departure(train)
@@ -25,9 +26,9 @@ class Station
 			p "Куда хотите отправить поезд? (f/b)"
 			answer = gets.chomp
 			if answer == 'f'
-				train.move_forward
+				return train.move_forward
 			elsif answer == 'b'
-				train.move_back
+				return train.move_back
 			end
 		else
 			p "Этого поезда нет на станции!"
@@ -47,7 +48,7 @@ class Route
 
 	def insert_station(station)
 		unless @stations.include?(station) then
-			@stations.insert(-2, station)
+			return @stations.insert(-2, station)
 		end
 
 	end
@@ -59,14 +60,14 @@ class Route
 	end
 
 	def show_stations		
-		@stations.each { |station| puts station.name}
-		puts ''
+		return @stations.each { |station| puts station.name}
 	end
+
 end
 
 class Train
 
-	attr_reader :name, :train_type, :car_quantity, :speed, :train_station
+	attr_reader :speed, :name, :car_quantity, :train_type, :train_route, :train_station, :index_station
 
 	def initialize(name, train_type, car_quantity = 0)
 		@name = name
@@ -79,19 +80,19 @@ class Train
 
 	def accelerate(speed)
 		if speed > 0 
-			p @speed += speed
+			return @speed += speed
 		else 
 			p "Введите положительное число!"	
 		end
 	end
 
 	def stop
-		@speed= 0
+		return @speed= 0
 	end
 
 	def add_car		
 		if @speed == 0 
-			 @car_quantity += 1
+			 return @car_quantity += 1
 		else
 			p "Снизьте скорость до нуля!"
 		end		
@@ -100,12 +101,12 @@ class Train
 	def remove_car
 		if self.speed == 0
 			if self.car_quantity > 0
-				@car_quantity -= 1
+				return @car_quantity -= 1
 			else
-				p "В этом поезде уже нет вагонов!"
+				return "В этом поезде уже нет вагонов!"
 			end
 		else
-			p "Снизьте скорость до нуля!"
+			return "Снизьте скорость до нуля!"
 		end	 
 	end	
 
@@ -113,6 +114,8 @@ class Train
 		@train_route = route
 		@train_station = route.stations[0] 
 		@train_route.stations[0].trains_list << self
+		return @train_route
+
 	end
 
 	def move_forward
@@ -122,12 +125,12 @@ class Train
 					@train_station = @train_route.stations[i += 1]
 					@train_route.stations[i].trains_list << self
 					@train_route.stations[i-1].trains_list.delete(self)
-					puts "Поезд отправился на станцию #{@train_station.name}"	
+					return @train_station
 				else
-				p "Вы достигли конечной станции!"	
+				return "Вы достигли конечной станции!"	
 				end
 		else
-			p "Чтобы двигаться, увеличте скорость!"
+			return "Чтобы двигаться, увеличте скорость!"
 		end	
 	end
 
@@ -138,30 +141,32 @@ class Train
 				@train_station = @train_route.stations[i -= 1]
 				@train_route.stations[i].trains_list << self
 				@train_route.stations[i+1].trains_list.delete(self)
-				puts "Поезд отправился на станцию #{@train_station.name}"
+				return @train_station
 			else
-				p "Вы достигли начальной станции!"	
+				return "Вы достигли начальной станции!"	
 			end
 		else
-			p "Чтобы двигаться, увеличте скорость!"
+			return "Чтобы двигаться, увеличте скорость!"
 		end	
 	end
 
 	def previous_station
 		i = @train_route.stations.index(@train_station)
 		if i > 0
-			p "Предыдущая станция на маршруте поезда #{self.name}:  #{@train_route.stations[i-1].name}"
+			return @train_route.stations[i-1].name
 		else
-			p "Вы находитесь на начальной станции!"
+			return "Вы находитесь на начальной станции!"
 		end
+
 	end
 
 	def next_station
 		i = @train_route.stations.index(@train_station)
 		if i < (@train_route.stations.size - 1)
-			p "Следующая станция на маршруте поезда #{self.name}:  #{@train_route.stations[i+1].name}"
+			return @train_route.stations[i+1].name
 		else
-			p "Вы находитесь на конечной станции!"
+			return "Вы находитесь на конечной станции!"
 		end
+
 	end	
 end
