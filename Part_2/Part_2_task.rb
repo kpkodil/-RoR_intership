85aae079f1b64dcf3b3a7b77635ae700c37a68b7
kpkodil <kpkopb@gmail.com>
2020-11-07 12:03:14 +0300

    Update Part_2_task.rb

--- Part_2/Part_2_task.rb
+++ Part_2/Part_2_task.rb
@@ -52,8 +52,6 @@ class Train
     @train_type = train_type
     @car_quantity = car_quantity
     @speed = 0
-    @train_route = train_route
-    @train_station = train_station
   end
 
   def accelerate(speed)
@@ -88,13 +86,19 @@ class Train
   def move_forward
     return unless speed.positive? && next_station
 
+    next_station.trains_list << self
     @train_station = next_station
+    previous_station.trains_list.delete(self)
+    @train_station
   end
 
   def move_back
     return unless speed.positive? && previous_station
 
+    previous_station.trains_list << self
     @train_station = previous_station
+    next_station.trains_list.delete(self)
+    @train_station
   end
 
   def previous_station
